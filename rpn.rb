658606c3867e8f111607b091c5e472fbde8f8f48
Phil Runninger (mac) <prunninger@virtualhold.com>
2018-03-01 14:31:04 -0500

    Add command-line processing for quick calculations

--- rpn.rb
+++ rpn.rb
@@ -7,42 +7,48 @@ require_relative 'processor'
 
 processor = Processor.new
 
-puts "RPN Calculator, ©2016, Phil Runninger ".colorize(processor.settings.color_title) + 
-  "#{'═' * (processor.console_columns - 57)} Enter ? for help.".colorize(processor.settings.color_help)
-input = ''
-begin
+if ARGV.length > 0
+  answer = processor.execute(ARGV.join(' '))
+  puts processor.format(answer).colorize(processor.settings.color_normal)
+  Clipboard.copy processor.format(answer).to_s unless answer.nil?
+else
+  puts "RPN Calculator, ©2016, Phil Runninger ".colorize(processor.settings.color_title) + 
+    "#{'═' * (processor.console_columns - 57)} Enter ? for help.".colorize(processor.settings.color_help)
+  input = ''
   begin
-    answer = processor.execute(input)
-  rescue Exception => exception
-    answer = nil
-    puts exception.message.colorize(processor.settings.color_error)
+    begin
+      answer = processor.execute(input)
+    rescue Exception => exception
+      answer = nil
+      puts exception.message.colorize(processor.settings.color_error)
+    end
+
+    prompt = ''
+
+    prompt += "Recording #{processor.recording}... ".colorize(processor.settings.color_error) unless processor.recording.nil?
+    prompt += "#{processor.macros.keys.join(', ')}  ".colorize(processor.settings.color_help) unless processor.macros == {}
+
+    prompt += processor.registers.map{|name, value|
+      name.colorize(processor.settings.color_register) +
+        '='.colorize(processor.settings.color_normal) +
+        processor.format(value).colorize(processor.settings.color_register)}.join(' ')
+    prompt += ' • '.colorize(processor.settings.color_help_heading) unless processor.registers.empty?
+
+    prompt += processor.stack.map{|value| "#{processor.format(value)}" }.join(' ').colorize(processor.settings.color_normal)
+    prompt += ' ' unless processor.stack.empty?
+
+    prompt += processor.radix.colorize(processor.settings.color_title) + ' ' unless processor.radix.empty?
+
+    prompt += "#{processor.angle}► ".colorize(processor.settings.color_title)
+
+    input = Readline.readline(prompt.strip, true)
+    Readline::HISTORY.pop if Readline::HISTORY.length>1 &&  Readline::HISTORY[-1] == Readline::HISTORY[-2]
+  end while input > ''
+
+  puts "#{'═' * (processor.console_columns - 23)} Thanks for using rpn.".colorize(processor.settings.color_help)
+  if !answer.nil?
+    Clipboard.copy processor.format(answer).to_s
+    puts processor.format(answer).colorize(processor.settings.color_normal) +
+      ' was copied to the clipboard.'.colorize(processor.settings.color_register)
   end
-
-  prompt = ''
-
-  prompt += "Recording #{processor.recording}... ".colorize(processor.settings.color_error) unless processor.recording.nil?
-  prompt += "#{processor.macros.keys.join(', ')}  ".colorize(processor.settings.color_help) unless processor.macros == {}
-
-  prompt += processor.registers.map{|name, value|
-    name.colorize(processor.settings.color_register) +
-    '='.colorize(processor.settings.color_normal) +
-    processor.format(value).colorize(processor.settings.color_register)}.join(' ')
-  prompt += ' • '.colorize(processor.settings.color_help_heading) unless processor.registers.empty?
-
-  prompt += processor.stack.map{|value| "#{processor.format(value)}" }.join(' ').colorize(processor.settings.color_normal)
-  prompt += ' ' unless processor.stack.empty?
-
-  prompt += processor.radix.colorize(processor.settings.color_title) + ' ' unless processor.radix.empty?
-
-  prompt += "#{processor.angle}► ".colorize(processor.settings.color_title)
-
-  input = Readline.readline(prompt.strip, true)
-  Readline::HISTORY.pop if Readline::HISTORY.length>1 &&  Readline::HISTORY[-1] == Readline::HISTORY[-2]
-end while input > ''
-
-puts "#{'═' * (processor.console_columns - 23)} Thanks for using rpn.".colorize(processor.settings.color_help)
-if !answer.nil?
-  Clipboard.copy processor.format(answer).to_s
-  puts processor.format(answer).colorize(processor.settings.color_normal) +
-    ' was copied to the clipboard.'.colorize(processor.settings.color_register)
 end
