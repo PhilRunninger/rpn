c4db4dd380ceb6e6916e19d2756d919614f52f5f
Phil Runninger (mac) <prunninger@vhtcx.com>
2018-07-16 09:53:22 -0400

    Wrote module to loop until the user quits with empty string.

---
+++ erl/rpn.erl
@@ -0,0 +1,18 @@
+-module(rpn).
+
+-export([start/0]).
+
+%%% API
+
+start() ->
+    io:format("Starting RPN~n"),
+    loop(-1,[]).
+
+loop(0, _) ->
+    io:format("Thanks for using RPN.~n");
+loop(_, Stack) ->
+    lists:foreach(fun(X) -> io:format("~p ", [X]) end, lists:reverse(Stack)),
+    String = string:trim(string:chomp(io:get_line("> "))),
+    loop(length(String), processor:execute(String, Stack)).
+
+%%% Internal functions
