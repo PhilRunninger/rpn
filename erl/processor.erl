96e4b6ccdc27c8892a23e81926c73c5657485cdd
Phil Runninger (mac) <prunninger@vhtcx.com>
2018-10-05 09:38:31 -0400

    Replace repeated function name rpn with internal function.
    This was an idea that Dane and I had kicked around for simplifying a
    function with multiple pattern matches. Instead of this:
    
            foobar(a) -> 1;
            foobar(b) -> 2;
            ...
            foobar(z) -> 26.
    
    we replace it with something that has only one foobar:
    
            foobar(Arg) ->
                F = fun(a) -> 1;
                       (b) -> 2;
                       ...
                       (z) - 26
                    end,
                F(Arg).
    
    I think this contributes greatly to improved readability, especially if
    the outer function name is long and descriptive.

--- erl/processor.erl
+++ erl/processor.erl
@@ -19,53 +19,56 @@ loop([Input|T], Stack) ->
 
 %%% Internal functions
 
-rpn("+",     [X,Y|S]) -> [Y+X|S];
-rpn("-",     [X,Y|S]) -> [Y-X|S];
-rpn("*",     [X,Y|S]) -> [Y*X|S];
-rpn("/",     [X,Y|S]) -> [Y/X|S];
-rpn("div",   [X,Y|S]) -> [trunc(Y/X)|S];
-rpn("%",     [X,Y|S]) -> [Y rem X|S];
-rpn("**",    [X,Y|S]) -> [math:pow(Y,X)|S];
-rpn("chs",   [X  |S]) -> [-X|S];
+rpn(Operator, Stack) ->
+    F = fun("+",     [X,Y|S]) -> [Y+X|S];
+           ("-",     [X,Y|S]) -> [Y-X|S];
+           ("*",     [X,Y|S]) -> [Y*X|S];
+           ("/",     [X,Y|S]) -> [Y/X|S];
+           ("div",   [X,Y|S]) -> [trunc(Y/X)|S];
+           ("%",     [X,Y|S]) -> [Y rem X|S];
+           ("**",    [X,Y|S]) -> [math:pow(Y,X)|S];
+           ("chs",   [X  |S]) -> [-X|S];
 
-rpn("abs",   [X  |S]) -> [if X<0 -> -X; true -> X end|S];
+           ("abs",   [X  |S]) -> [if X<0 -> -X; true -> X end|S];
 
-rpn("pi",         S ) -> [math:pi()|S];
-rpn("e",          S ) -> [math:exp(1)|S];
-rpn("phi",        S ) -> [(math:sqrt(5)+1)/2|S];
+           ("pi",         S ) -> [math:pi()|S];
+           ("e",          S ) -> [math:exp(1)|S];
+           ("phi",        S ) -> [(math:sqrt(5)+1)/2|S];
 
-rpn("sin",   [X  |S]) -> [math:sin(X*math:pi()/180)|S];
-rpn("cos",   [X  |S]) -> [math:cos(X*math:pi()/180)|S];
-rpn("tan",   [X  |S]) -> [math:tan(X*math:pi()/180)|S];
-rpn("asin",  [X  |S]) -> [math:asin(X)*180/math:pi()|S];
-rpn("acos",  [X  |S]) -> [math:acos(X)*180/math:pi()|S];
-rpn("atan",  [X  |S]) -> [math:atan(X)*180/math:pi()|S];
+           ("sin",   [X  |S]) -> [math:sin(X*math:pi()/180)|S];
+           ("cos",   [X  |S]) -> [math:cos(X*math:pi()/180)|S];
+           ("tan",   [X  |S]) -> [math:tan(X*math:pi()/180)|S];
+           ("asin",  [X  |S]) -> [math:asin(X)*180/math:pi()|S];
+           ("acos",  [X  |S]) -> [math:acos(X)*180/math:pi()|S];
+           ("atan",  [X  |S]) -> [math:atan(X)*180/math:pi()|S];
 
-rpn("sqrt",  [X  |S]) -> [math:sqrt(X)|S];
-rpn("\\",    [X  |S]) -> [1/X|S];
-rpn("exp",   [X  |S]) -> [math:exp(X)|S];
-rpn("log",   [X  |S]) -> [math:log(X)|S];
-rpn("log2",  [X  |S]) -> [math:log2(X)|S];
-rpn("log10", [X  |S]) -> [math:log10(X)|S];
+           ("sqrt",  [X  |S]) -> [math:sqrt(X)|S];
+           ("\\",    [X  |S]) -> [1/X|S];
+           ("exp",   [X  |S]) -> [math:exp(X)|S];
+           ("log",   [X  |S]) -> [math:log(X)|S];
+           ("log2",  [X  |S]) -> [math:log2(X)|S];
+           ("log10", [X  |S]) -> [math:log10(X)|S];
 
-rpn("round", [X  |S]) -> [round(X)|S];
-rpn("trunc", [X  |S]) -> [trunc(X)|S];
-rpn("floor", [X  |S]) -> [round(math:floor(X))|S];
-rpn("ceil",  [X  |S]) -> [round(math:ceil(X))|S];
+           ("round", [X  |S]) -> [round(X)|S];
+           ("trunc", [X  |S]) -> [trunc(X)|S];
+           ("floor", [X  |S]) -> [round(math:floor(X))|S];
+           ("ceil",  [X  |S]) -> [round(math:ceil(X))|S];
 
-rpn("copy",  [X  |S]) -> [X,X|S];
-rpn("del",   [_  |S]) -> S;
-rpn("cs",    _      ) -> [];
-rpn("xy",    [X,Y|S]) -> [Y,X|S];
+           ("copy",  [X  |S]) -> [X,X|S];
+           ("del",   [_  |S]) -> S;
+           ("cs",    _      ) -> [];
+           ("xy",    [X,Y|S]) -> [Y,X|S];
 
-rpn("&",     [X,Y|S]) -> [X band Y|S];
-rpn("|",     [X,Y|S]) -> [X bor Y|S];
-rpn("^",     [X,Y|S]) -> [X bxor Y|S];
-rpn("~",     [X  |S]) -> [bnot X|S];
-rpn("<<",    [X,Y|S]) -> [Y bsl X|S];
-rpn(">>",    [X,Y|S]) -> [Y bsr X|S];
+           ("&",     [X,Y|S]) -> [X band Y|S];
+           ("|",     [X,Y|S]) -> [X bor Y|S];
+           ("^",     [X,Y|S]) -> [X bxor Y|S];
+           ("~",     [X  |S]) -> [bnot X|S];
+           ("<<",    [X,Y|S]) -> [Y bsl X|S];
+           (">>",    [X,Y|S]) -> [Y bsr X|S];
 
-rpn(Arg,          S ) -> [to_num(Arg)|S].
+           (Arg,          S ) -> [to_num(Arg)|S]
+        end,
+    F(Operator, Stack).
 
 to_num(Arg) ->
     case string:to_float(Arg) of
