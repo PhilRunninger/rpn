a7160c34eec2fa43c3eee79ffd3a072ec826cce3
Phil Runninger (mac) <prunninger@vhtcx.com>
2018-11-12 17:35:06 -0500

    Display complex numbers in the stack without the tuple's braces.

--- erl/rpn.erl
+++ erl/rpn.erl
@@ -11,7 +11,10 @@ start() ->
 loop(0, _) ->
     io:format("Thanks for using RPN.~n");
 loop(_, Stack) ->
-    lists:foreach(fun(X) -> io:format("~p ", [X]) end, lists:reverse(Stack)),
+    lists:foreach(fun({A,B}) -> io:format("~p,~p  ", [A,B]);
+                     (X) -> io:format("~p  ", [X])
+                  end,
+                  lists:reverse(Stack)),
     String = string:trim(string:chomp(io:get_line("> "))),
     loop(length(String), processor:execute(String, Stack)).
 
